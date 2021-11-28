890f26266917baa82ae9b2582b1bba9a9c139383
jdlugosz963 <jdlugosz963@gmail.com>
2021-11-28 22:57:10 +0100

    Add groups list and detail view

--- chat_api/serializers.py
+++ chat_api/serializers.py
@@ -3,6 +3,15 @@ from rest_framework import serializers
 from .models import *
 
 class GroupSerializer(serializers.ModelSerializer):
+    is_mine = serializers.SerializerMethodField('_is_mine')
+
+    def _is_mine(self, obj):
+        request = self.context.get('request')
+        if request:
+            return request.user == obj.owner
+        return False
+
     class Meta:
         model = Group
         fields = '__all__'
+        extra_fields = ('is_mine', )
